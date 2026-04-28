bc0d19fd0c945ddccb2d8cd93a59a56e210328fb
JuanDC10 <juancastpar@unisabana.edu.co>
2026-04-27 21:52:16 -0500

    1.4

--- todo_manager.py
+++ todo_manager.py
@@ -1,11 +1,17 @@
 """Laboratorio 8 - Módulo de persistencia para lista de tareas."""
 
 
+
 def read_todo_file(file_path):
     """Reads tasks from a file. Returns a list of tasks."""
-    # TODO: Implementar manejo de FileNotFoundError según README.md
-    raise NotImplementedError
-
+    try:
+        file = open(file_path, 'r')
+        tareas = file.read().splitlines()
+        file.close()
+        return tareas
+    except FileNotFoundError:
+        print(f"File {file_path} not found! Returning an empty to-do list.")
+        return []
 
 def write_todo_file(file_path, tasks):
     """Writes tasks to a file, one per line."""
