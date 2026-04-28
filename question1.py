5540cee66fcb8f2cc88a672180db1da1a72edc7d
JuanDC10 <juancastpar@unisabana.edu.co>
2026-04-27 21:20:02 -0500

    1.1

--- question1.py
+++ question1.py
@@ -4,3 +4,7 @@ Implementa una CLI que calcule carga por punto de soporte.
 """
 
 # TODO: Implementar según README.md
+import sys
+
+def calcular_carga(total_load, num_supports):
+    return total_load / num_supports
